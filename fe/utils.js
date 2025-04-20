b1e0d29bcdcbab85649ab23cd743ca78170e178e
Galang <galangrakha2@gmail.com>
2025-04-20 19:16:59 +0700

    first

---
+++ fe/utils.js
@@ -0,0 +1,9 @@
+const API_CONFIG = {
+  DEFAULT_URL: 'https://doker-263444552508.us-central1.run.app/api',
+  
+  // Get the API URL
+  getApiUrl: function() {
+    console.log('Current API URL:', this.DEFAULT_URL);
+    return this.DEFAULT_URL;
+  }
+};
